d1f22cc426a3de4365d768bac3f8533396c39769
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-04-09 19:15:54 +0200

    add item scriptable

---
+++ Frogies-Project/Assets/Scripts/Items/Scriptable/ItemScriptable.cs
@@ -0,0 +1,18 @@
+﻿using Items.Data;
+using UnityEngine;
+
+namespace Items.Scriptable
+{
+    [CreateAssetMenu(fileName = "Item", menuName = "ItemsSystem/Item")]
+    public class ItemScriptable : BaseItemScriptable
+    {
+        [SerializeField] private ItemDescriptor _itemDescriptor;
+        public override ItemDescriptor ItemDescriptor => _itemDescriptor;
+        
+        public ItemScriptable(ItemDescriptor descriptor)
+        {
+            _itemDescriptor = descriptor;
+        }
+
+    }
+}

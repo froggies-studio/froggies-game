00c93245967442819184f66eb234e0d787c2b8a4
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-04-23 20:34:39 +0200

    + stat changing descriptor and item scriptable

---
+++ Frogies-Project/Assets/Scripts/Items/Data/StatChangingItemDescriptor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Items.Enum;
+using StatsSystem;
+using UnityEngine;
+
+namespace Items.Data
+{
+    [Serializable]
+    public class StatChangingItemDescriptor : ItemDescriptor
+    {
+        [field: SerializeField] public List<StatModifier> StatModifiers { get; private set; }
+
+        public StatChangingItemDescriptor(
+            ItemId itemId, ItemMaterial material, ItemType type, 
+            Sprite itemSprite, ItemRarity itemRarity, float price, List<StatModifier> modifiers) : 
+            base(itemId, material, type, itemSprite, itemRarity, price)
+        {
+            StatModifiers = modifiers;
+        }
+    }
+}

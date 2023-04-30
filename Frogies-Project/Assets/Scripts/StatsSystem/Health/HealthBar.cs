40602493fe31f760cb2b57c0088be0d2c50e18f8
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-04-30 19:38:51 +0200

    add health bar

---
+++ Frogies-Project/Assets/Scripts/StatsSystem/Health/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Core;
+using StatsSystem.Enum;
+using UnityEngine;
+
+namespace StatsSystem.Health
+{
+    public class HealthBar : MonoBehaviour
+    {
+        private StatsController _statsController;
+        
+        public void Setup(StatsController statsController)
+        {
+            _statsController = statsController;
+            _statsController.OnStatChanged += StatsController_OnStatChanged;
+        }
+
+        private void StatsController_OnStatChanged(Stat stat)
+        {
+            if (stat.Type == StatType.Health)
+            {
+                float percent = stat.Value / _statsController.GetStatsValue(StatType.MaxHealth);
+                transform.Find("Scale").localScale = new Vector3(percent, 1);
+
+            }
+        }
+    }
+}

980245591bdc0ed7533ffc885d21c9f217e1043b
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-05-03 18:48:22 +0200

    fixed health and endurance bars

--- Frogies-Project/Assets/Scripts/StatsSystem/Endurance/EnduranceControlBar.cs
+++ Frogies-Project/Assets/Scripts/StatsSystem/Endurance/EnduranceControlBar.cs
@@ -2,20 +2,22 @@
 using StatsSystem.Enum;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace StatsSystem.Endurance
 {
     public class EnduranceControlBar : MonoBehaviour
     {
-        [SerializeField] private Transform _scale;
-        [SerializeField] private TMP_Text _currentHealthText;
+        [SerializeField] private Slider scale;
+        [SerializeField] private TMP_Text currentEnduranceText;
         private StatsController _statsController;
         
         public void Setup(StatsController statsController)
         {
             _statsController = statsController;
             _statsController.OnStatChanged += StatsController_OnStatChanged;
-            _currentHealthText.text = _statsController.GetStatsValue(StatType.MaxEndurance).ToString();
+            currentEnduranceText.text = _statsController.GetStatsValue(StatType.MaxEndurance).ToString();
         }
 
         private void StatsController_OnStatChanged(Stat stat)
@@ -23,8 +25,8 @@ namespace StatsSystem.Endurance
             if (stat.Type == StatType.Endurance)
             {
                 float percent = stat.Value / _statsController.GetStatsValue(StatType.MaxEndurance);
-                _scale.localScale = new Vector3(percent, 1);
-                _currentHealthText.text = Math.Round(stat.Value).ToString();
+                scale.value = percent;
+                currentEnduranceText.text = Math.Round(stat.Value).ToString();
             }
         }
     }

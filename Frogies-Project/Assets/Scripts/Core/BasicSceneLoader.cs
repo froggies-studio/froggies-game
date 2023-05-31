6085bedaf3286abae03c9f41b5b7b53c295fee95
Mariia Kapshuk <marichka.v.richtsi@gmail.com>
2023-05-31 22:42:25 +0200

    add trigger scene loader

--- Frogies-Project/Assets/Scripts/Core/BasicSceneLoader.cs
+++ Frogies-Project/Assets/Scripts/Core/BasicSceneLoader.cs
@@ -1,26 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Core
 {
-    [RequireComponent(typeof(Collider2D))]
     public class BasicSceneLoader: MonoBehaviour
     {
-        [SerializeField] private SceneAsset nextScene;
-        [SerializeField] private Collider2D entityCollider;
+        [SerializeField] protected SceneAsset nextScene;
 
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            if (other.Equals(entityCollider))
-            {
-                LoadScene();
-            }
-        }
-
-        private void LoadScene()
+        protected void LoadScene()
         {
             SceneManager.LoadScene(nextScene.name);
         }
